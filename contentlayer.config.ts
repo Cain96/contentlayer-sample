69287e2e85cf8270b0ac3f0d064ed2f54cc4f277
Kenta Kuroda <kenta.kuroda@route06.co.jp>
2022-05-24 21:33:15 +0900

    Add remark plugins

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,4 +1,10 @@
 import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import rehypeStringify from "rehype-stringify";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkGfm from "remark-gfm";
+import remarkParse from "remark-parse";
+import remark2rehype from "remark-rehype";
+import { Processor } from "unified";
 
 export const Post = defineDocumentType(() => ({
   name: "Post",
@@ -37,4 +43,12 @@ export const Post = defineDocumentType(() => ({
 export default makeSource({
   contentDirPath: "posts",
   documentTypes: [Post],
+  markdown: (builder: Processor) => {
+    builder
+      .use(remarkFrontmatter)
+      .use(remarkParse)
+      .use(remarkGfm)
+      .use(remark2rehype, { allowDangerousHtml: true, footnoteLabel: "脚注", footnoteBackLabel: "戻る" })
+      .use(rehypeStringify, { allowDangerousHtml: true });
+  },
 });
--- posts/post-01.md
+++ posts/post-01.md
@@ -9,3 +9,15 @@ tags: ["タグ1", "タグ2"]
 これはブログ 1 のテストです。
 
 私はすべて一々その焦燥ようにおいてののためと要らましませ。かつて多年に開始人ももちろんこうした意見ますならだけへなりが来たがも希望きまっますうと、そうにも被せるますだでです。身拵えをあるでものはちゃんとほかがいやいやませたた。いくらでも嘉納さんに出立がただんだん誤解をあります味大した理科それか努力へとしてご説明ざるたんでて、どんな事実は私か説時代がもたらすて、岡田君のものから資格の私をちょうど今納得としてそれ秩序がお観念がしようにいくらご教育を飲んですないで、勢いちょうど唱道に立つたてみでのにできうでし。
+
+> hogehoge
+
+```
+codeです。
+```
+
+Here is a simple footnote[^1]. With some additional text after it.
+
+[^1]: My reference.
+
+<img src='https://1.bp.blogspot.com/-tVeC6En4e_E/X96mhDTzJNI/AAAAAAABdBo/jlD_jvZvMuk3qUcNjA_XORrA4w3lhPkdQCNcBGAsYHQ/s1048/onepiece01_luffy.png' width=300>
